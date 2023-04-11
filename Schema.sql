34c1dbf1f505640a9b53ad547e8de6c41e57f26d
Aryan <f20210532@pilani.bits-pilani.ac.in>
2023-04-11 15:28:45 +0530

    Login Implemented

--- Schema.sql
+++ Schema.sql
@@ -1,61 +1,58 @@
-CREATE TABLE Contact (
-    contact_id INT PRIMARY KEY,
+CREATE TABLE User (
+    user_id INT PRIMARY KEY auto_increment,
     name VARCHAR(50),
     email VARCHAR(50),
-    phone_number VARCHAR(20)
+    phone_number VARCHAR(20),
+    password VARCHAR(255),
+    authority VARCHAR(20) DEFAULT 'user'
 );
 
-CREATE TABLE User (
-    user_id INT PRIMARY KEY,
-    contact_id INT,
-    FOREIGN KEY (contact_id) REFERENCES Contact(contact_id)
-);
- 
 CREATE TABLE Driver (
-    driver_id INT PRIMARY KEY,
+    driver_id INT PRIMARY KEY auto_increment,
     type_id INT,
-    contact_id INT,
+    name VARCHAR(50),
+    email VARCHAR(50),
+    phone_number VARCHAR(20),
     license_plate VARCHAR(20),
-    status ENUM('available', 'booked') DEFAULT 'available',
-    FOREIGN KEY (type_id) REFERENCES Car_Type(type_id),
-    FOREIGN KEY (contact_id) REFERENCES Contact(contact_id)
+    status VARCHAR(20) DEFAULT 'available',
+    FOREIGN KEY (type_id) REFERENCES Car_Type(type_id) on update cascade on delete cascade
 );
 
 CREATE TABLE Car_Type (
-    type_id INT PRIMARY KEY,
+    type_id INT PRIMARY KEY auto_increment,
     price INT,
     passengers INT
 );
 
 CREATE TABLE Car (
-    car_id INT PRIMARY KEY,
+    car_id INT PRIMARY KEY auto_increment,
     type_id INT,
     driver_id INT,
-    FOREIGN KEY (type_id) REFERENCES Car_Type(type_id),
-    FOREIGN KEY (driver_id) REFERENCES Driver(driver_id)
+    FOREIGN KEY (type_id) REFERENCES Car_Type(type_id) on update cascade on delete cascade,
+    FOREIGN KEY (driver_id) REFERENCES Driver(driver_id) on update cascade on delete cascade
 );
 
 CREATE TABLE Rides (
-    ride_id INT PRIMARY KEY,
+    ride_id INT PRIMARY KEY auto_increment,
     user_id INT,
     car_id INT,
     pickup_location VARCHAR(50),
     dropoff_location VARCHAR(50),
     booking_time TIMESTAMP,
-    ride_status ENUM('booked', 'waiting') DEFAULT 'booked',
-    rating ENUM(1, 2, 3, 4, 5),
-    desc VARCHAR(255),
+    ride_status VARCHAR(20) DEFAULT 'waiting', /* or booked if driver has been allocated */
+    rating INT,
+    description VARCHAR(255),
     payment_id INT,
-    FOREIGN KEY (user_id) REFERENCES User(user_id),
-    FOREIGN KEY (car_id) REFERENCES Car(car_id),
-    FOREIGN KEY (payment_id) REFERENCES Payment(payment_id)
+    FOREIGN KEY (user_id) REFERENCES User(user_id) on update cascade on delete cascade,
+    FOREIGN KEY (car_id) REFERENCES Car(car_id) on update cascade on delete cascade
 );
 
+
 CREATE TABLE Payment (
-    payment_id INT PRIMARY KEY,
+    payment_id INT PRIMARY KEY auto_increment,
     ride_id INT,
     gateway VARCHAR(255),
     user_id INT,
-    FOREIGN KEY (ride_id) REFERENCES Rides(ride_id),
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
+    FOREIGN KEY (ride_id) REFERENCES Rides(ride_id) on update cascade on delete cascade,
+    FOREIGN KEY (user_id) REFERENCES User(user_id) on update cascade on delete cascade
 );
---
+++ src/Utility.java
@@ -0,0 +1,119 @@
+import javax.swing.*;
+import java.sql.*;
+
+public class Utility {
+
+    ConnectionClass obj;
+
+    Utility() {
+        obj = new ConnectionClass();
+    }
+
+    public int Login(String email, String password, String authority) {
+        try {
+            String query = "select * from User where email = '" + email + "' and password = '" + password
+                    + "' and authority = '" + authority + "'";
+            ResultSet rs = obj.stm.executeQuery(query);
+            if (rs.next()) {
+                System.out.println("Login Successful");
+                return 1;
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid Login Details");
+                return 0;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return 0;
+    }
+
+    public int RegisterUser(String email, String name, String password, String phone) {
+        try {
+            String q1 = "insert into user(name, password, email, phone_number) values('" + name + "','" + password
+                    + "','" + email + "','" + phone + "');";
+            int aa = obj.stm.executeUpdate(q1);
+            if (aa == 1) {
+                JOptionPane.showMessageDialog(null, "Account Created Successfully");
+                return 1;
+            } else {
+                JOptionPane.showMessageDialog(null, "Please enter valid details");
+                return 0;
+            }
+        } catch (SQLIntegrityConstraintViolationException e) {
+            JOptionPane.showMessageDialog(null, "User already exists");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return 0;
+    }
+
+    public int RegisterAdmin(String email, String name, String password, String phone) {
+        try {
+            String q1 = "insert into user(name, password, email, phone_number, authority) values('" + name + "','"
+                    + password
+                    + "','" + email + "','" + phone + "', 'admin');";
+            int aa = obj.stm.executeUpdate(q1);
+            if (aa == 1) {
+                JOptionPane.showMessageDialog(null, "Account Created Successfully");
+                return 1;
+            } else {
+                JOptionPane.showMessageDialog(null, "Please enter valid details");
+                return 0;
+            }
+        } catch (SQLIntegrityConstraintViolationException e) {
+            JOptionPane.showMessageDialog(null, "User already exists");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return 0;
+    }
+
+    public int RegisterDriver(int type_id, String name, String email, String phone, String license_plate,
+            String status) {
+        try {
+            String q1 = "insert into driver(type_id, name, email, phone_number, license_plate, status) values("
+                    + type_id + ",'" + name + "','" + email + "','" + phone + "','" + license_plate + "','" + status
+                    + "');";
+            int aa = obj.stm.executeUpdate(q1);
+            if (aa == 1) {
+                JOptionPane.showMessageDialog(null, "Account Created Successfully");
+                return 1;
+            } else {
+                JOptionPane.showMessageDialog(null, "Please enter valid details");
+                return 0;
+            }
+
+        } catch (
+
+        SQLIntegrityConstraintViolationException e) {
+            JOptionPane.showMessageDialog(null, "Driver already exists");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return 0;
+    }
+
+    public int InsertCar(int type_id, String name, String email, String phone, String license_plate, String status) {
+        try {
+            String q1 = "insert into cab(type_id, name, email, phone_number, license_plate, status) values("
+                    + type_id + ",'" + name + "','" + email + "','" + phone + "','" + license_plate + "','" + status
+                    + "');";
+            int aa = obj.stm.executeUpdate(q1);
+            if (aa == 1) {
+                JOptionPane.showMessageDialog(null, "Account Created Successfully");
+                return 1;
+            } else {
+                JOptionPane.showMessageDialog(null, "Please enter valid details");
+                return 0;
+            }
+
+        } catch (
+
+        SQLIntegrityConstraintViolationException e) {
+            JOptionPane.showMessageDialog(null, "Cab already exists");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return 0;
+    }
+}
